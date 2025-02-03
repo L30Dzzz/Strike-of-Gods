7cdce2a66d8d1a612665b4f55bd43bd78a56533f
eclipse2101 <113185639+eclipse2101@users.noreply.github.com>
2025-02-03 14:09:18 -0500

    Getting somewhere with the code for hitbox poperties
    going to push this to my branch (red)

--- Assets/Gameplay Scripts/hitProperties.cs
+++ Assets/Gameplay Scripts/hitProperties.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class hitProperties : MonoBehaviour
 {
+   PlayerMovement Player;
+   
     // Start is called before the first frame update
     void Start()
     {
-        
+        Player = GetComponentInParent<PlayerMovement>();
     }
 
     // Update is called once per frame
