445433edfdde9f5505cce08944a3febe40ec7dc3
eclipse2101 <113185639+eclipse2101@users.noreply.github.com>
2025-02-28 14:09:01 -0500

    made the win condition work

--- Assets/Gameplay Scripts/HealthBar.cs
+++ Assets/Gameplay Scripts/HealthBar.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
     private int basehp;
+    private int basehp2;
     public GameObject p1Health;
     public GameObject p2Health; 
     public GameObject p1Meter; 
@@ -19,21 +21,40 @@ public class HealthBar : MonoBehaviour
     void Awake()
     {
         
+        
+        basehp = (int)p1Health.GetComponent<Image>().rectTransform.rect.width;
+        basehp2 = (int)p2Health.GetComponent<Image>().rectTransform.rect.width;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(basehp <= 0)
+        {
+           StartCoroutine(gameCondition(KoScreen, P1WinScreen, MenuScreen));
+        }
+        else if(basehp2 <= 0)
+        {
+            StartCoroutine(gameCondition(KoScreen, P2WinScreen, MenuScreen));
+        }
+
+
     }
 
     
-     private IEnumerator gameCondition(GameObject WinScreen, GameObject Menu)
+     private IEnumerator gameCondition(GameObject Ko, GameObject WinScreen, GameObject Menu)
     {
        
-       
+       Ko.SetActive(true);
 
-       
-       return null;
+       yield return new WaitForSeconds(2);
+
+       Ko.SetActive(false);
+       WinScreen.SetActive(true);
+
+       yield return new WaitForSeconds(2);
+
+       WinScreen.SetActive(false);
+       Menu.SetActive(true);
     }
 }
