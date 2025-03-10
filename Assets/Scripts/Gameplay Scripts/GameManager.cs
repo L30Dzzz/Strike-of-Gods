b7264f325167895aff41f82d4672a1ecca202074
Kassie730 <dmcpherson25@aoiths.org>
2025-03-10 13:23:56 -0400

    Cache Push/Trailer and Rounds

---
+++ Assets/Scripts/Gameplay Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem; 
+
+
+public class GameManager : MonoBehaviour
+{
+
+    public static bool gameIsPaused;
+    public GameObject PauseSystem;
+    public GameObject player1;
+    public GameObject player2;
+   // public TextMeshProGUI roundEndText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+     void Update()
+    {
+            PauseGame();
+    }
+
+    public void Paused(InputAction.CallbackContext context)
+    {
+       gameIsPaused = !gameIsPaused; 
+    }
+    
+    void PauseGame ()
+    {
+        if(gameIsPaused)
+        {
+            Time.timeScale = 0f;
+            PauseSystem.gameObject.SetActive(true);
+        }
+        else 
+        {
+            Time.timeScale = 1;
+            PauseSystem.gameObject.SetActive(false);
+        }
+    }
+
+  //  void RoundEnd()
+   // {
+        
+   // }
+}
