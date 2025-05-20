1adc3cb3040f52823e2ce407a404749d47002b23
eclipse2101 <113185639+eclipse2101@users.noreply.github.com>
2025-05-20 14:08:11 -0400

    fixing up the sprites for wukong walking animation

--- Assets/Scripts/Gameplay Scripts/HealthBar.cs
+++ Assets/Scripts/Gameplay Scripts/HealthBar.cs
@@ -124,10 +124,6 @@ public class HealthBar : MonoBehaviour
             
         p1HealthBar.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, basehp);
         p2HealthBar.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, basehp2);
-        /*
-        Debug.Log(basehp);
-        Debug.Log(basehp2);
-        */
         
      }
 
@@ -188,7 +184,7 @@ public class HealthBar : MonoBehaviour
                 Menu.SetActive(true);
             }
             
-       }
+     }
 
     }
 
@@ -207,14 +203,17 @@ public class HealthBar : MonoBehaviour
             if(basehp > basehp2) // if player one wins 
             {
                 StartCoroutine(gameCondition(KoScreen, P1WinScreen, MenuScreen, P1Points, P1Score));
+                isRunning = false;
                 Debug.Log("Player one wins ");
             }
             else if(basehp2 > basehp) // if player 2 wins 
             {
                 StartCoroutine(gameCondition(KoScreen, P2WinScreen, MenuScreen, P2Points, P2Score));
+                isRunning = false;
                 Debug.Log("Player 2 wins");
             }
 
+            StopCoroutine(gameTimer());
             Debug.Log("Times up");
 
         }
